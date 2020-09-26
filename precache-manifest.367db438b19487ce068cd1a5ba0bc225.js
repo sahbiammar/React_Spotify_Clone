947ce1b95424679a3920cad4216b5673720e4da4
sahbiammar <sahbi.ammar.1995@gmail.com>
2020-09-26 17:59:48 +0200

    Updates

---
+++ precache-manifest.367db438b19487ce068cd1a5ba0bc225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef89ea0aa79256f92e5d25e17aa44932",
+    "url": "/React_Spotify_Clone/index.html"
+  },
+  {
+    "revision": "f9e6e869a4b450b9d61c",
+    "url": "/React_Spotify_Clone/static/css/main.4a8b6ae5.chunk.css"
+  },
+  {
+    "revision": "3299c6ca1933c3f9b82e",
+    "url": "/React_Spotify_Clone/static/js/2.209b4482.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React_Spotify_Clone/static/js/2.209b4482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9e6e869a4b450b9d61c",
+    "url": "/React_Spotify_Clone/static/js/main.cdc9f12a.chunk.js"
+  },
+  {
+    "revision": "c27dafb63590260b52ac",
+    "url": "/React_Spotify_Clone/static/js/runtime-main.e10a83fe.js"
+  }
+]);
